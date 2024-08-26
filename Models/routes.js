0d9d3d3023b8c65d9b866772ae4fa9ddae097ea2
LucasShouji <140670816+LucasShouji@users.noreply.github.com>
2024-08-26 03:30:08 -0300

    Delete Models/routes.js

--- Models/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const EnderecoController = require('./controllers/EnderecoController');
-
-
-router.post('/endereco/:cep', EnderecoController.buscarESalvarEndereco);
-
-module.exports = router;
