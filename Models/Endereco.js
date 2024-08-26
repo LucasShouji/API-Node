48ad6bb13751e9834c5fb731823b049104906437
LucasShouji <140670816+LucasShouji@users.noreply.github.com>
2024-08-26 10:26:34 -0300

    Update Endereco.js

--- Models/Endereco.js
+++ Models/Endereco.js
@@ -1,4 +1,4 @@
-const {Sequelize, DataTypes} = require('sequelize');
+/*const {Sequelize, DataTypes} = require('sequelize');
 const sequelize = new Sequelize('sqlite::memory');
 
 const User = sequelize.define(
@@ -53,4 +53,54 @@ const User = sequelize.define(
         }
     }
     
-);
+);*/
+const { Model, DataTypes, Sequelize } = require('sequelize');
+/*const Sequelize = require('sequelize');
+const { type } = require('os');
+const { isNumberObject } = require('util/types');*/
+const sequelize = new Sequelize('postgres://postgres:banco123@localhost:5432/apinode');
+
+class Endereco extends Model {}
+
+Endereco.init({
+        Id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        Cep: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Numero: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        Complemento: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Bairro: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Cidade: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Estado: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        MunicipioIBGE: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    }, {
+        sequelize,
+        modelName: 'Endereco',
+        tableName: 'enderecos',
+        timestamps: true
+    });
+
+    module.exports = Endereco;
